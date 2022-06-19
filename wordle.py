9be80fc5f5aad14d48ce3c1790a600a2d58d6742
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-06-19 15:57:58 +0530

    c0.4.0 class Wordle

--- wordle.py
+++ wordle.py
@@ -4,77 +4,82 @@ import io
 import random
 import rich
 
-def letter_correct(letter):
-    """Return green letter"""
-    return f'[black on green]{letter}[/]'
+class Wordle():
+    """Main class"""
+    @staticmethod
+    def letter_correct(letter):
+        """Return green letter"""
+        return f'[black on green]{letter}[/]'
 
-def letter_present(letter):
-    """Return yellow letter"""
-    return f'[black on yellow]{letter}[/]'
+    @staticmethod
+    def letter_present(letter):
+        """Return yellow letter"""
+        return f'[black on yellow]{letter}[/]'
 
-def letter_absent(letter):
-    """Return gray letter"""
-    return f'[black on gray]{letter}[/]'
+    @staticmethod
+    def letter_absent(letter):
+        """Return gray letter"""
+        return f'[black on gray]{letter}[/]'
 
-def check_guess(word, guess):
-    """Validates guess"""
-    coloring = []
-    word_letters = list(word)
-    for i in range(5):
-        letter = guess[i]
-        if letter in word_letters:
-            word_letters.remove(letter)
-            if letter == word[i]:
-                coloring.append(letter_correct(letter))
+    def validate(self, guess):
+        """Validates guess"""
+        coloring = []
+        word_letters = list(self.word)
+        for i in range(5):
+            letter = guess[i]
+            if letter in word_letters:
+                word_letters.remove(letter)
+                if letter == self.word[i]:
+                    coloring.append(self.letter_correct(letter))
+                else:
+                    coloring.append(self.letter_present(letter))
             else:
-                coloring.append(letter_present(letter))
-        else:
-            coloring.append(letter_absent(letter))
-    return coloring
+                coloring.append(self.letter_absent(letter))
+        return coloring
 
-def game_start():
-    """Initialise the game"""
-    with io.open('five_letter_words.txt', 'r', encoding='utf8') as file:
-        five_letter_words = file.read().splitlines()
-    word = random.choice(five_letter_words)
-    # print("Word: ", word)
-    return five_letter_words, word
+    def __init__(self):
+        """Initialise the game"""
+        with io.open('five_letter_words.txt', 'r', encoding='utf8') as file:
+            self.five_letter_words = file.read().splitlines()
+        self.word = random.choice(self.five_letter_words)
+        # print("Word: ", self.word)
+        self.won = False
+        self.state = 1
+        self.guesses = []
+        self.game_loop()
 
-def game_loop(five_letter_words, word):
-    """Main game loop"""
-    won = False
-    state = 1
-    guesses = []
-    while won is False:
-        print('Try:', state)
-        for guess in guesses:
-            coloring = check_guess(word, guess)
+    def game_loop(self):
+        """Main game loop"""
+        while self.won is False:
+            print("-" * 25)
+            print('Try:', self.state)
+            for guess in self.guesses:
+                coloring = self.validate(guess)
+                rich.print(''.join(coloring))
+            guess = input()
+            if len(guess) != 5:
+                print("Try again. Enter 5 letter words.")
+                continue
+            if guess in self.guesses:
+                print("You have already guessed that word.")
+                continue
+            if guess not in self.five_letter_words:
+                print("Enter a valid word. Try again.")
+                continue
+            self.guesses.append(guess)
+            if guess == self.word:
+                self.won = True
+                print('You win! Guessed it in', self.state, 'tries!')
+                break
+            coloring = self.validate(guess)
             rich.print(''.join(coloring))
-        guess = input()
-        if len(guess) != 5:
-            print("Try again. Enter 5 letter words.")
-            continue
-        if guess in guesses:
-            print("You have already guessed that word.")
-            continue
-        if guess not in five_letter_words:
-            print("Enter a valid word. Try again.")
-            continue
-        guesses.append(guess)
-        if guess == word:
-            won = True
-            print('You win! Guessed it in', state, 'tries!')
-            break
-        coloring = check_guess(word, guess)
-        rich.print(''.join(coloring))
-        state = state + 1
-        if state > 6:
-            print("Better luck next time! The word was", word)
-            break
+            self.state += 1
+            if self.state > 6:
+                print("Better luck next time! The word was", self.word)
+                break
 
 def main():
     """Main"""
-    five_letter_words, word = game_start()
-    game_loop(five_letter_words, word)
+    Wordle()
 
 main()
