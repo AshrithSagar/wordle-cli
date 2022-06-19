753064e8e5e4c91e771cf2ad91a76c221404170f
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-06-20 00:48:00 +0530

    c0.6.2 Panel width

--- wordle.py
+++ wordle.py
@@ -48,7 +48,7 @@ class Wordle():
             for guess in self.guesses:
                 coloring.append(self.validate(guess))
                 coloring.append('\n')
-            rprint(Panel(coloring, title="Wordle", subtitle="Try:"+str(self.state)))
+            rprint(Panel(coloring, title="Wordle", subtitle="Try:"+str(self.state), width=12))
             guess = input().lower()
             if len(guess) != 5:
                 print("Try again. Enter 5 letter words.")
